75690fb4021462554fed10b544d8480bc64db2ea
RafaelBlanco00 <rafaelblanco00@gmail.com>
2025-05-27 17:45:09 -0500

    Puerta
    Se agrego una puerta para programar su funcionamiento

---
+++ RBlancoP1/Assets/Codigos/PuertaControlador.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertaControlador : MonoBehaviour
+{
+    
+
+    private void Awake()
+    {
+        
+    }
+
+    public void AbrirCerrarPuerta()
+    {
+        
+    }
+
+}
